34bd71554fc27be2b14c34ab04ae8955893450b0
Maria Kury <mariaskury@gmail.com>
2023-06-02 18:22:34 -0300

    New design

--- public/home.js
+++ public/home.js
@@ -1,26 +1,29 @@
 $(document).ready(() => {
-  $('body').addClass('loaded_hiding')
+  $("body").addClass("loaded_hiding");
   setTimeout(() => {
-    $('body').addClass('loaded')
-    $('body').removeClass('loaded_hiding')
-  }, 500)
-})
+    $("body").addClass("loaded");
+    $("body").removeClass("loaded_hiding");
+  }, 500);
+});
 
-$('.about__button').click(() => {
-  $('.popup-about').addClass('_active')
-  $('body').addClass('_lock')
-})
+$(".about__button").click(() => {
+  $(".popup-about").addClass("_active");
+  $("body").addClass("_lock");
+});
 
-$('.popup-about__close').click(() => {
-  $('.popup-about').removeClass('_active')
-  $('body').removeClass('_lock')
-})
+$(".popup-about__close").click(() => {
+  $(".popup-about").removeClass("_active");
+  $("body").removeClass("_lock");
+});
 
-$('.arrow._down').click(() => {
-  $('.modules')[0].scrollIntoView({ behavior: 'smooth' })
-})
+$(".arrow._down").click(() => {
+  $(".modules")[0].scrollIntoView({ behavior: "smooth" });
+});
 
-$('.arrow._up').click(() => {
-  $('.wrapper')[0].scrollIntoView({ behavior: 'smooth' })
-})
+$(".arrow._up").click(() => {
+  $(".wrapper")[0].scrollIntoView({ behavior: "smooth" });
+});
 
+$(".dark-mode-button").click(() => {
+  $("body").toggleClass("_dark-mode");
+});
